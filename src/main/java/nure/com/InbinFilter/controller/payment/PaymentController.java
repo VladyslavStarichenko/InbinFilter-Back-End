4f0b6b79c5b942d9ac578cc6f7ca18b8b9bd6c5b
goldsilver12 <vladyslav.starichenko@nure.ua>
2021-12-21 20:37:59 +0200

    Scope increasing

--- src/main/java/nure/com/InbinFilter/controller/payment/PaymentController.java
+++ src/main/java/nure/com/InbinFilter/controller/payment/PaymentController.java
@@ -44,9 +44,9 @@ public class PaymentController {
             Resident residentDb = resident.get();
 
             Charge charge = this.stripeClient.chargeNewCard(token, amount);
-            if(charge !=null){
+
                 residentDb.setBill(0.0);
-            }
+
             return charge;
         }
        throw  new CustomException("Payment was failed", HttpStatus.BAD_REQUEST);
